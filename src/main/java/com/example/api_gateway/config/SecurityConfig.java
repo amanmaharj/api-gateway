607cea827782495c12c75861bcf3b0dfb4ee3fd2
amanmaharj <aman.mahaj@gmail.com>
2025-02-28 20:42:49 -0500

    added the extra route for handling specific documentation route through api gateway,refactoring to authorize http requests for the documentation site

--- src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -8,9 +8,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private final String[] resourceNoBoundToAuthenticate = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resource/**" , "/api-docs/**", "/aggregate/**"};
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-      return httpSecurity.authorizeHttpRequests(authorize->authorize.anyRequest()
+      return httpSecurity.authorizeHttpRequests(authorize->authorize.requestMatchers(resourceNoBoundToAuthenticate).permitAll()
+                      .anyRequest()
                 .authenticated())
                 .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
                 .build();
--- src/main/java/com/example/api_gateway/routes/Route.java
+++ src/main/java/com/example/api_gateway/routes/Route.java
@@ -1,12 +1,19 @@
 package com.example.api_gateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
 @Configuration
 public class Route {
@@ -15,9 +22,17 @@ public class Route {
     public RouterFunction<ServerResponse> productServiceRoute(){
         return GatewayRouterFunctions.route("ktmshop")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
-
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                        .filter(setPath("/api-docs"))
+                .build();
+    }
     @Bean
     //functional endpoint programming model
     public RouterFunction<ServerResponse> orderServiceRoute(){
@@ -27,9 +42,31 @@ public class Route {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute(){
         return GatewayRouterFunctions.route("inventory_service")
                 .route(RequestPredicates.path("/api/inventory"),HandlerFunctions.http("http://localhost:8082"))
                 .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"),HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute(){
+        return route("fallbackRoute")
+                .GET("/fallback", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                        .body("Service Unavailable, please try again later")).build();
     }
 }
